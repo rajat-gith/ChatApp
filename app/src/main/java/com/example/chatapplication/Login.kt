596f268f5ca1a69a8d3702d74ac30606e292d07b
Rajat Kumar Nayak <rajatnayak1582002@gmail.com>
2022-06-06 22:43:00 +0530

    Login & Signup Commit

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -1,12 +1,61 @@
 
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.ActionCodeEmailInfo
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+
 
 class Login : AppCompatActivity() {
+    private lateinit var mAuth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val SignupBtn=findViewById<Button>(R.id.SignUp)
+        val LoginBtn=findViewById<Button>(R.id.Login)
+
+        mAuth= FirebaseAuth.getInstance()
+
+        SignupBtn.setOnClickListener {
+            val intent= Intent(this,com.example.chatapplication.SignUp::class.java)
+            startActivity(intent)
+        }
+
+        LoginBtn.setOnClickListener {
+            val email=email.text.toString()
+            val password=password.text.toString()
+            if(check()){
+                mAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this){task->
+                        if(task.isSuccessful) {
+                            Toast.makeText(this, "LoginSuccess", Toast.LENGTH_SHORT).show()
+
+                        }else{
+                            Toast.makeText(this,"Some Error Occured",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            }else{
+                Toast.makeText(this,"Empty Fields",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    private fun check():Boolean{
+        if(email.text.toString().trim { it<=' ' }.isNotEmpty()
+            && password.toString().trim{it<=' '}.isNotEmpty()){
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/com/example/chatapplication/SignUp.kt
+++ app/src/main/java/com/example/chatapplication/SignUp.kt
@@ -1,11 +1,49 @@
 package com.example.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUp : AppCompatActivity() {
+    private lateinit var mAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth=FirebaseAuth.getInstance()
+        val btnlogin=findViewById<Button>(R.id.Loginbtn)
+
+        btnlogin.setOnClickListener {
+            val register_email=register_email.text.toString()
+            val registerpassword=register_password.text.toString()
+            if(checking()){
+                mAuth.createUserWithEmailAndPassword(register_email,registerpassword)
+                    .addOnCompleteListener(this){ task->
+                        if(task.isSuccessful()){
+                            Toast.makeText(this,"Account Created",Toast.LENGTH_SHORT).show()
+                            val intent= Intent(this,MainActivity::class.java)
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(this,"Some Error Occured",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this,"Empty Fields",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun checking():Boolean{
+        if(register_email.text.toString().trim { it<' ' }.isNotEmpty()
+            && register_password.text.toString().trim { it<' ' }.isNotEmpty()
+            && name.text.toString().trim { it<' ' }.toString().isNotEmpty())
+        {
+            return true
+        }
+        return false
     }
 }
